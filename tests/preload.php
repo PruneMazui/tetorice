34ff9a386593001b517bc2dbf097343076f6f4fb
tanaka <step.by.step524@gmail.com>
2017-10-31 03:16:11 +0900

    PHPUnit version for php56

--- tests/preload.php
+++ tests/preload.php
@@ -1,5 +1,5 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$reflection = new \ReflectionClass(\PHPUnit\Framework\Assert::class);
+$reflection = new \ReflectionClass(\PHPUnit_Framework_Assert::class);
 require_once dirname($reflection->getFileName()) . '/Assert/Functions.php';
